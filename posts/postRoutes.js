081536b4182ddb61836bbab3f19c1e663bf748f9
ajb85 <ajbrush85@gmail.com>
2019-03-20 17:00:56 -0400

    delete added for api/posts

--- posts/postRoutes.js
+++ posts/postRoutes.js
@@ -29,4 +29,17 @@ router.post("/", async (req, res) => {
   }
 });
 
+router.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await db.remove(id);
+    user
+      ? res.status(200).end()
+      : res.status(404).json({ message: "No post by that id" });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ message: "Error deleting the post" });
+  }
+});
+
 module.exports = router;
