9daa84d41c610b53adab36d70a97f6cb67c64c90
ajb85 <ajbrush85@gmail.com>
2019-03-20 16:23:09 -0400

    Added delete for api/users

--- users/userRoutes.js
+++ users/userRoutes.js
@@ -15,12 +15,26 @@ router.post("/", async (req, res) => {
   try {
     if (req.body.name) {
       const newPost = await db.insert(req.body);
-      res.status(201).json(req.body);
+      res.status(201).json(newPost);
     } else {
       res.status(400).json({ message: "Please provide a name." });
     }
   } catch (error) {
+    console.log(error);
     res.status(500).json({ message: "Error posting to database" });
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const remove = await db.remove(id);
+    res.status(200).json(remove);
+  } catch (err) {
+    console.log(err);
+    res
+      .status(500)
+      .json({ message: "Everything exploded, sorry.  Try again." });
   }
 });
 
